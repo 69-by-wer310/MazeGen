5d52d8b13e97f2a2fce45248b979f827511308bd
wer310 <77055011+wer310@users.noreply.github.com>
2022-05-26 11:20:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from slogger import SLogger
 import argparse
 from PIL import Image
 from mazelib import Maze
 from mazelib.generate.Prims import Prims
+logger = SLogger()
 args = argparse.ArgumentParser()
 args.add_argument("-w", "--w", type=int, default=0)
 args.add_argument("-d", "--hi", type=int, default=0)
@@ -10,9 +12,14 @@ args = args.parse_args()
 (w, h, name) = (args.w, args.hi, args.name)
 if w == 0 or h == 0 or name == "":
 	(w, h, name) = (int(input("w:")), int(input("h:")), input("name:"))
+logger.info("Creating maze...")
 m = Maze()
+logger.info("done!")
 m.generator = Prims(w, h)
+logger.info("Generating maze...")
 m.generate()
+logger.info("done!")
+logger.info("saving maze...")
 maze = str(m)
 mazelist = maze.split("\n")
 #for x in mazelist:
@@ -24,3 +31,5 @@ for x in range(img.size[0]):
 		if pixel == "#":
 			img.putpixel((x, y), (0, 0, 0))
 img.save(name+".png", "PNG")
+logger.info("done!")
+logger.info("END")
