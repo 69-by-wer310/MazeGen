38b28c3516b74b2cc0114a14ed11c2ca45f75c40
wer310 <77055011+wer310@users.noreply.github.com>
2022-05-26 10:10:04 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+gen maze!
+```
+pip3 inatall -r requirements.txt
+python3 main.py
+```
+
---
+++ main.py
@@ -0,0 +1,26 @@
+import argparse
+from PIL import Image
+from mazelib import Maze
+from mazelib.generate.Prims import Prims
+args = argparse.ArgumentParser()
+args.add_argument("-w", "--w", type=int, default=0)
+args.add_argument("-d", "--hi", type=int, default=0)
+args.add_argument("-n", "--name", type=str, default="")
+args = args.parse_args()
+(w, h, name) = (args.w, args.hi, args.name)
+if w == 0 or h == 0 or name == "":
+	(w, h, name) = (int(input("w:")), int(input("h:")), input("name:"))
+m = Maze()
+m.generator = Prims(w, h)
+m.generate()
+maze = str(m)
+mazelist = maze.split("\n")
+#for x in mazelist:
+#	mazelist[x] = mazelist[x].replace("\n", "")
+img = Image.new("RGB", (len(mazelist), len(mazelist[0])), color=(255, 255, 255))
+for x in range(img.size[0]):
+	for y in range(img.size[1]):
+		pixel = mazelist[x][y]
+		if pixel == "#":
+			img.putpixel((x, y), (0, 0, 0))
+img.save(name+".png", "PNG")
